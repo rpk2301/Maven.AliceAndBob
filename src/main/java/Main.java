934ae38f72b417db77924c05290b7c45bacc9724
ricky <rpk2301@vt.edu>
2023-06-28 17:13:28 -0400

    I have finished the lab!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+
+        String name = "";
+        String Alice = "Alice";
+        String Bob = "Bob";
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        name = scan.nextLine();
+
+
+        //If the name entered is bob or alice, return the print statement welcoming them.
+        if (name.equals(Bob)|| name.equals(Alice))
+        {
+        System.out.println("Hello, " + name);
+        }
 
     }
 }
+
+
+
+
+
